889defed51478ecdf77f8417799f0faa6f69a0ab
Mingjie Li <mingjli@ethz.ch>
2022-05-15 16:46:13 +0200

    6.5.1

--- NewclassExtension/ClassExtension.cpp
+++ NewclassExtension/ClassExtension.cpp
@@ -9,37 +9,56 @@ double Calculator::average_slow(const std::vector<double>& x){
 double Calculator::fluctuation_slow(const std::vector<double>& x){
     double Q = 0.;
     double av = average_slow(x);
-    for(int i = 0; i < numMDSteps; ++i){
+    int size = x.size();
+    for(int i = 0; i < size; ++i){
         Q += (x[i]-av)*(x[i]-av);
     }
-    return Q/(numMDSteps-1);
+    return std::sqrt(Q/(numMDSteps-1));
 }
 
-double Calculator::inaccurate_fluctuation(const std::vector<double>& x){
-    double Ssq = 0; // sum of x squared
-    double S = 0; // sum of x
-    for(int i = 0; i < numMDSteps; ++i){
-        S += x[i];
-        Ssq += x[i]*x[i];
-    }
-    return (Ssq - S*S/numMDSteps)/(numMDSteps -1);
+void Calculator::inaccurate_fluctuation(double x_i, int m){
+    S(m) += x_i;
+    Ssq(m) += std::pow(x_i, 2);
 }
 
-std::pair<double, double> Calculator::AverageFluctuation_fast(const std::vector<double>& x){
-    double S = 0.;
-    double Q = 0.;
-    for(int i = 0; i < numMDSteps; ++i){
-       int I = i+1;
-       Q += std::pow((S-(I-1)*x[i]), 2)/(I*(I-1));
-       S += x[i];
-    }
-    auto ret = std::make_pair(S/numMDSteps, Q/(numMDSteps-1));
+void Calculator::accurate_fluctuation(double x_i, int m){
+    Q(m) += std::pow((S-(index-1)*x_i),2)/(index*(index-1));
+    S(m) += x_i;
+    ++index;
+}
 
-    return ret;
+double Calculator::getAverage(int m){
+    return S(m)/numMDSteps;
+}
+
+double Calculator::getFluctuation(int m){
+    if(accurate){
+        return getFluctuation_accurate(m);
+    } else {
+        return getFluctuation_inaccurate(m);
+    }
+}
+
+double Calculator::getFluctuation_inaccurate(int m){
+    double var = (Ssq(m)-S(m)*S(m)/numMDSteps)/(numMDSteps-1);
+    return std::sqrt(var); 
+}
+
+double Calculator::getFluctuation_accurate(int m){
+    double var = Q(m)/(numMDSteps-1);
+    return std::sqrt(var); 
+}
+
+void Calculator::computeAverageFluctuation(double x_i, int m){
+    if(accurate){
+        accurate_fluctuation(x_i, m);
+    } else {
+        inaccurate_fluctuation(x_i, m);
+    }
 }
 
 std::vector<double> Calculator::C_direct(const std::vector<double>& x){
-    std::vector<double> ret;
+    std::vector<double> ret(numMDSteps);
 
     for(int n = 0; n < numMDSteps; ++n){
         double S = 0.;
--- NewclassExtension/ClassExtension.hpp
+++ NewclassExtension/ClassExtension.hpp
@@ -3,29 +3,50 @@
 
 #include <vector>
 #include <cmath>
+#include <Eigen/Dense>
 
 class Calculator{
 
 public:
 
-    Calculator(int numberAtoms, int numberMDSteps): N_x(numberAtoms), numMDSteps(numberMDSteps){}
+    Calculator(int numberMDSteps, int numberProperties): index(2), accurate(false)
+                numMDSteps(numberMDSteps), numberProperties(numberProperties){
+        S.setZero(numberProperties);
+        Q.setZero(numberProperties);
+        Ssq.setZero(numberProperties);
+    }
 
     double average_slow(const std::vector<double>& x); 
     double fluctuation_slow(const std::vector<double>& x);
     
     // formula 42
-    double inaccurate_fluctuation(const std::vector<double>& x);
+    void inaccurate_fluctuation(double x_i, int m);
     
     // pair[0]: average, pair[1]:fluctuation
-    std::pair<double, double> AverageFluctuation_fast(const std::vector<double>& x);
+    void accurate_fluctuation(double x_i, int m);
+
+    double getAverage(int m);
+    double getFluctuation(int m);
+    double getFluctuation_inaccurate(int m);
+    double getFluctuation_accurate(int m);
+
+    void computeAverageFluctuation(double x_i, int m);
+    void set_mode(bool is_accurate){
+        accurate = is_accurate;
+    }
 
     //Correlation function
     std::vector<double> C_direct(const std::vector<double>& x);
 
 private:
 
-    int N_x;
+    int index;
     int numMDSteps;
+    int numberProperties;
+    bool accurate;
+    Eigen::VectorXd S;
+    Eigen::VectorXd Q;
+    Eigen::VectorXd Ssq; // sum of pow(x_i,2)
         
 };
 
