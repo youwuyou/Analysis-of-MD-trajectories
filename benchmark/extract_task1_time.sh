3079763b81dd87fc1a89e484874cfff88cd46029
Mingjie Li <mingjli@ethz.ch>
2022-05-22 01:26:18 +0200

    corrected general version

--- benchmark/extract_task1_time.sh
+++ benchmark/extract_task1_time.sh
@@ -5,8 +5,7 @@ echo "#timesteps  time" > task1_time
 for numatm in 100 200 400 1000 2000
 do
 
-	dt=$(awk -v na=$numatm 'BEGIN {print 20/na } ')
-	printf "${dt}" >> task1_time
+	printf "${numatm}" >> task1_time
 	
 	awk 'NR == 69 { 
   		print " " $8
